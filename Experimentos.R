6ae4ac0283b26faeee9ed8a241f621c85692dbea
JacintoCC <jacintocc0@gmail.com>
2017-04-26 11:30:19 +0200

    Experimentos paralelos. Ineficiencia menos sangrante

--- Experimentos.R
+++ Experimentos.R
@@ -9,14 +9,27 @@ make.experiment <- function(train, test, parameters){
 
 make.experiment(tra, tst, parameters = list(k = 2))
 
-param.grid <- expand.grid(k = seq(2,40,by=4),
+param.grid <- expand.grid(k = seq(2,40, by = 4),
                           initialization = 0.1,
-                          lr = seq(0.0005,0.002, length.out = 4),
+                          lr = c(0.0005,0.001),
                           gamma = seq(0.01, 0.03, length.out = 3),
-                          max.iter= c(10,50),
+                          max.iter= 10,
                           verbose = F)
+param <- mapply(list, 
+                k = param.grid$k, 
+                initialization = param.grid$initialization,
+                lr = param.grid$lr,
+                gamma = param.grid$gamma,
+                max.iter = param.grid$max.iter,
+                verbose = param.grid$verbose,
+                SIMPLIFY = F)
 
-results <- apply(param.grid, 1, function(param.combination){
-   print(param.combination)
-   make.experiment(tra,tst,param.combination)
-})
+library(pbmcapply)
+results <- pbmclapply(param, function(param.combination){
+   timestamp()
+   cat(unlist(param.combination))
+   single.res <- make.experiment(tra,tst,
+                                 parameters = append(param.combination, 
+                                                     list(actualization.feature = SGD.feature)))
+}, mc.cores = 4,mc.preschedule = T, mc.silent = F)
+
--- FunkSVD.R
+++ FunkSVD.R
@@ -1,22 +1,46 @@
+#'@function SGD for feature
+#'@description Execute stochastic gradient descent on a feature of a ratings matrix and update U and Vt matrices
+#'
+#'@param R Ratings matrix
+#'@param U U matrix in the pseudo SVD decomposition
+#'@param Vt t(V) matrix in the pseudo SVD decomposition
+#'@param f Feature number to apply convergence
+#'@param lr Learning rate
+#'@param gamma Regularization term
+#'@param baseline Baseline prediction matrix
+#'@return List of U and t(V) matrices
 SGD.feature <- function(R, U, Vt,f,
                         lr, gamma,
                         baseline){
-   for(a in sample(nrow(R))){
-      for(i in sample(ncol(R))){
-         r_ai <- R[a,i]
-         if(!is.na(r_ai)){
-            p_ai <- baseline[a,i] + U[a, ] %*% Vt[ ,i]
-            e_ai <- r_ai - p_ai
-            temp_u_af <- U[a,f]
-            U[a,f] <- U[a,f] + lr * (e_ai * Vt[f,i] - gamma * U[a,f])
-            Vt[f,i] <- Vt[f,i] + lr * (e_ai * temp_u_af - gamma * Vt[f,i])
-         }
+   
+   not.ai <- which(!is.na(R), arr.ind = T)
+   for(j in nrow(not.ai)){
+      a <- not.ai[j,1]
+      i <- not.ai[j,2]
+      r_ai <- R[a,i]
+      if(!is.na(r_ai)){
+         p_ai <- baseline[a,i] + U[a, ] %*% Vt[ ,i]
+         e_ai <- r_ai - p_ai
+         temp_u_af <- U[a,f]
+         U[a,f] <- U[a,f] + lr * (e_ai * Vt[f,i] - gamma * U[a,f])
+         Vt[f,i] <- Vt[f,i] + lr * (e_ai * temp_u_af - gamma * Vt[f,i])
       }
    }
    
-   return(list(U, Vt))
+   return(list(U = U, Vt = Vt))
 }
 
+#'@function GD for feature
+#'@description Execute gradient descent on a feature of a ratings matrix and update U and Vt matrices
+#'
+#'@param R Ratings matrix
+#'@param U U matrix in the pseudo SVD decomposition
+#'@param Vt t(V) matrix in the pseudo SVD decomposition
+#'@param f Feature number to apply convergence
+#'@param lr Learning rate
+#'@param gamma Regularization term
+#'@param baseline Baseline prediction matrix
+#'@return List of U and t(V) matrices
 GD.feature <- function(R, U, Vt,f,
                        lr, gamma,
                        baseline){
@@ -33,7 +57,11 @@ GD.feature <- function(R, U, Vt,f,
    return(list(U = U,Vt = Vt))
 }
 
-
+#'@function Baseline
+#'@description Compute Baseline predictions
+#'
+#'@param R Ratings matrix
+#'@return Baseline predictions
 baseline <- function(R){
    offset.users <- apply(tra, 1, mean, na.rm = T) - mean(apply(tra, 1, mean, na.rm = T), na.rm = T)
    baseline.movies <- apply(tra, 2, mean, na.rm = T)
@@ -95,5 +123,5 @@ error.prediction <- function(original, funk, train){
    baseline <- baseline(train)
    prediction <- baseline + funk$U %*% t(funk$V)
    # RMSE
-   sqrt(mean((original-prediction)^2,na.rm=T))
+   sqrt(mean((original-prediction)^2, na.rm=T))
 }
